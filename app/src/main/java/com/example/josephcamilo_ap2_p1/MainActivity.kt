33cf206b9cede6f6df1ab0e59e54255e12d9f24c
josep <joseph_camilo@ucne.edu.do>
2025-10-01 22:41:40 -0400

    Modificando MainActivity

--- app/src/main/java/com/example/josephcamilo_ap2_p1/MainActivity.kt
+++ app/src/main/java/com/example/josephcamilo_ap2_p1/MainActivity.kt
@@ -4,44 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.example.josephcamilo_ap2_p1.data.local.database.EntradaDb
+import com.example.josephcamilo_ap2_p1.presentation.navigation.RegistroHuacalesNavHost
 import com.example.josephcamilo_ap2_p1.ui.theme.JosephCamilo_Ap2_P1Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var entradaDb: EntradaDb
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        entradaDb = Room.databaseBuilder(
+            applicationContext,
+            EntradaDb::class.java,
+            "Entrada.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
         setContent {
             JosephCamilo_Ap2_P1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { paddingValues ->
+                    Box(modifier = Modifier.padding(paddingValues)) {
+                        RegistroHuacalesNavHost(
+                            navHostController = rememberNavController()
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JosephCamilo_Ap2_P1Theme {
-        Greeting("Android")
     }
 }
